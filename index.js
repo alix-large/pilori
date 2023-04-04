9083698c77d86c7123eb6813dd13f36acb400a1f
Alix-Large <largealix@gmail.com>
2023-04-04 09:43:28 +0200

    installation des dépendances et du serveur

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('test');
+import express from 'express';
+import * as dotenv from "dotenv";
+import session from 'express-session';
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const app = express ();
+
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+    resave: true, // la session est réenregistrée meme si elle n'est pas modifiée
+    secret: process.env.APP_SECRET, // ajoute une part d'aléatoire dans la génération des id de session imprédictible
+    saveUninitialized: true, // génère un id de session pour tous ceux qui n'en ont pas encore
+}));
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+});
